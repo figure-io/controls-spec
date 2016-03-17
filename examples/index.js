f565ce1d1e509ec06d7f2c5b40476de87aa2f65a
Planeshifter <pburckhardt@outlook.com>
2016-03-16 22:37:59 -0400

    Style changes

--- examples/index.js
+++ examples/index.js
@@ -1,28 +1,28 @@
 'use strict';
 
-var validate = require( './../lib' ),
-	goodConfig,
-	badConfig,
-	isValid;
+var validate = require( './../lib' );
+var goodConfig;
+var badConfig;
+var isValid;
 
 // Load in our contol interface configurations...
 goodConfig = require( './good.json' );
 /*
 	{
-	    "id": 0,
-	    "type":"slider",
-	    "max": 10,
-	    "min": 0,
-	    "step":1
+		"id": 0,
+		"type":"slider",
+		"max": 10,
+		"min": 0,
+		"step":1
 	}
 */
 badConfig = require( './bad.json' );
 /*
 	{
-	    "id": 0,
-	    "type":"slider",
-	    "min":"wrong",
-	    "max":1
+		"id": 0,
+		"type":"slider",
+		"min":"wrong",
+		"max":1
 	}
 */
 
